cd6d1eb4bb2883dfccfa6456879636c3de986732
roisin <104171770+crispy-wonton@users.noreply.github.com>
2023-10-10 11:17:38 +0100

    merge asf_senedd_response wales_analysis.py
    add script to generate stats and charts from asf_senedd_response into produce_plots.py

--- asf_welsh_energy_consultation/analysis/produce_plots.py
+++ asf_welsh_energy_consultation/analysis/produce_plots.py
@@ -5,9 +5,16 @@ Script to produce plots.
 
 import altair as alt
 
-from asf_welsh_energy_consultation.getters.get_data import get_electric_tenure
+# from asf_welsh_energy_consultation.getters.get_data import get_electric_tenure
 from asf_welsh_energy_consultation.pipeline.process_data import *
 from asf_welsh_energy_consultation.utils.formatting import format_number
+from asf_welsh_energy_consultation.getters.get_data import load_wales_df, load_wales_hp
+from asf_welsh_energy_consultation.pipeline.augmenting import generate_age_data
+from asf_welsh_energy_consultation.pipeline.plotting import (
+    proportions_bar_chart,
+    age_prop_chart,
+    time_series_comparison,
+)
 
 from nesta_ds_utils.viz.altair.formatting import setup_theme
 
@@ -19,57 +26,6 @@ output_folder = "outputs/figures/"
 
 if not os.path.isdir(output_folder):
     os.makedirs(output_folder)
-
-
-def time_series_comparison(
-    data,
-    title,
-    y_var,
-    y_title,
-    color_var,
-    x_var="date:T",
-    x_title="Date",
-    domain_min="2015-01-01",
-    domain_max="2023-01-01",
-    width=600,
-    height=300,
-):
-    """Generic function for plotting a line chart by category (represented by color_var).
-
-    Args:
-        data (pd.DataFrame): Base data. Needs to be structured as a column of consecutive dates,
-            a column indicating categories and a column with cumulative values.
-        title (str/list): Chart title.
-        y_var (str): y variable.
-        y_title (str): y axis title.
-        color_var (str): Variable to split by.
-        x_var (str, optional): x variable. Defaults to "date:T".
-        x_title (str, optional): x axis title. Defaults to "Date".
-        domain_min (str, optional): x axis minimum. Defaults to "2015-01-01".
-        domain_max (str, optional): x axis maximum. Defaults to "2023-01-01".
-        width (int, optional): Chart width. Defaults to 600.
-        height (int, optional): Chart height. Defaults to 300.
-
-    Returns:
-        alt.Chart: Base altair chart.
-    """
-    chart = (
-        alt.Chart(
-            data,
-            title=title,
-        )
-        .mark_line()
-        .encode(
-            x=alt.X(
-                x_var, title=x_title, scale=alt.Scale(domain=[domain_min, domain_max])
-            ),
-            y=alt.Y(y_var, title=y_title),
-            color=color_var,
-        )
-        .properties(width=width, height=height)
-    )
-
-    return chart
 
 
 if __name__ == "__main__":
@@ -213,3 +169,195 @@ if __name__ == "__main__":
     ).configure_title(fontSize=20)
 
     electric_tenure_chart.save(output_folder + "electric_tenure.html")
+
+    # ======================================================
+    # Original plots and stats
+
+    wales_df = load_wales_df(from_csv=False)
+    wales_hp = load_wales_hp(wales_df)
+
+    # English plots
+
+    # Key statistics
+    print("Number of heat pumps:", len(wales_hp))
+    print("Number of properties in EPC:", len(wales_df))
+    print(
+        "Estimated percentage of properties with a heat pump:",
+        "{:.2%}".format(len(wales_hp) / len(wales_df)),
+    )
+    print(wales_hp.TENURE.value_counts(normalize=True))
+
+    epc_c_or_above_and_good_walls = wales_df.loc[
+        wales_df["CURRENT_ENERGY_RATING"].isin(["A", "B", "C"])
+        & wales_df["WALLS_ENERGY_EFF"].isin(["Good", "Very Good"])
+    ]
+
+    epc_c_or_above_and_good_walls_and_roof = epc_c_or_above_and_good_walls.loc[
+        epc_c_or_above_and_good_walls["ROOF_ENERGY_EFF"].isin(["Good", "Very Good"])
+    ]
+
+    print(
+        "Number of EPC C+ properties with good or very good wall insulation:",
+        len(epc_c_or_above_and_good_walls),
+    )
+    print(
+        "As a proportion of properties in EPC:",
+        len(epc_c_or_above_and_good_walls) / len(wales_df),
+    )
+
+    print(
+        "\nNumber of EPC C+ properties with good or very good wall and roof insulation:",
+        len(epc_c_or_above_and_good_walls_and_roof),
+    )
+    print(
+        "As a proportion of properties in EPC:",
+        len(epc_c_or_above_and_good_walls_and_roof) / len(wales_df),
+    )
+
+    # Tenure of Welsh HPs
+    proportions_bar_chart(
+        wales_hp,
+        "TENURE",
+        "Fig. 3: Tenure of Welsh properties with heat pumps",
+        "Tenure",
+        "Percentage of properties",
+        filename="hp_tenure",
+        x_type="tenure",
+        expand_y=True,
+    )
+
+    # EPC, all
+    proportions_bar_chart(
+        # only one unknown EPC property so fine to just remove it
+        wales_df.loc[wales_df.CURRENT_ENERGY_RATING != "unknown"],
+        "CURRENT_ENERGY_RATING",
+        "Fig. 5: EPC ratings of all Welsh properties",
+        "Energy efficiency rating",
+        "Percentage of properties",
+        filename="epc_all",
+        x_type="other",
+    )
+
+    # EPC, private sector with HPs
+    proportions_bar_chart(
+        wales_hp.loc[wales_hp.TENURE.isin(["Owner-occupied", "Privately rented"])],
+        "CURRENT_ENERGY_RATING",
+        [
+            "Fig. 6: EPC ratings of owner-occupied and privately rented",
+            "Welsh properties with heat pumps",
+        ],
+        "Energy efficiency rating",
+        "Percentage of properties",
+        filename="epc_hp_private",
+        x_type="other",
+    )
+
+    # EPCs, private sector with retrofitted HPs
+    proportions_bar_chart(
+        wales_hp.loc[
+            wales_hp.TENURE.isin(["Owner-occupied", "Privately rented"])
+            & (wales_hp.CONSTRUCTION_AGE_BAND != "2007 onwards")
+        ],
+        "CURRENT_ENERGY_RATING",
+        [
+            "Fig. 7: EPC ratings of owner-occupied and privately rented",
+            "Welsh properties with heat pumps, built pre-2007",
+        ],
+        "Energy efficiency rating",
+        "Percentage of properties",
+        filename="epc_hp_private_retrofit",
+        x_type="other",
+    )
+
+    age_data = generate_age_data(wales_df)
+    age_prop_chart(
+        age_data, "Fig. 9: Construction age bands and energy efficiencies", "age_prop"
+    )
+
+    ## Welsh plots
+
+    welsh_replacements = {
+        "TENURE": {
+            "Owner-occupied": "Perchen-feddiannaeth",
+            "Socially rented": "Rhentu cymdeithasol",
+            "Privately rented": "Rhentu preifat",
+            "Unknown": "Anhysbys",
+        },
+        "CONSTRUCTION_AGE_BAND": {
+            "England and Wales: before 1900": "Cyn 1900",
+            "Pre-1900": "Cyn 1900",
+            "2007 onwards": "2007 ymlaen",
+            "unknown": "Anhysbys",
+        },
+    }
+
+    for df in [wales_df, wales_hp, age_data]:
+        for col in ["TENURE", "CONSTRUCTION_AGE_BAND"]:
+            if col in df.columns:
+                df[col] = df[col].replace(welsh_replacements[col])
+
+    # Tenure of Welsh HPs
+    proportions_bar_chart(
+        wales_hp,
+        "TENURE",
+        "Ffig. 4: Deiliadaeth eiddo â phympiau gwres yng Nghymru",
+        "Deiliadaeth",
+        "Canran yr eiddo",
+        filename="hp_tenure_welsh",
+        x_type="tenure",
+        expand_y=True,
+        language="welsh",
+    )
+
+    # EPC, all
+    proportions_bar_chart(
+        wales_df.loc[wales_df.CURRENT_ENERGY_RATING != "unknown"],
+        "CURRENT_ENERGY_RATING",
+        "Ffig. 6: Sgoriau EPC holl eiddo Cymru",
+        "Sgôr effeithlonrwydd ynni",
+        "Canran yr eiddo",
+        filename="epc_all_welsh",
+        x_type="other",
+        language="welsh",
+    )
+
+    # EPC, private sector with HPs
+    proportions_bar_chart(
+        wales_hp.loc[wales_hp.TENURE.isin(["Perchen-feddiannaeth", "Rhentu preifat"])],
+        "CURRENT_ENERGY_RATING",
+        [
+            "Ffig. 7: Sgoriau EPC eiddo perchen-feddiannaeth a",
+            "rhentu preifat Cymru sydd â phympiau gwres",
+        ],
+        "Sgôr effeithlonrwydd ynni",
+        "Canran yr eiddo",
+        filename="epc_hp_private_welsh",
+        x_type="other",
+        language="welsh",
+    )
+
+    # EPCs, private sector with retrofitted HPs
+    proportions_bar_chart(
+        wales_hp.loc[
+            wales_hp.TENURE.isin(["Perchen-feddiannaeth", "Rhentu preifat"])
+            & (wales_hp.CONSTRUCTION_AGE_BAND != "2007 ymlaen")
+        ],
+        "CURRENT_ENERGY_RATING",
+        [
+            "Ffig. 8: Sgoriau EPC eiddo perchen-feddiannaeth a rhentu prifat",
+            "Cymru sydd â phympiau gwres, a adeiladwyd cyn 2007",
+        ],
+        "Sgôr effeithlonrwydd ynni",
+        "Canran yr eiddo",
+        filename="epc_hp_private_retrofit_welsh",
+        x_type="other",
+        language="welsh",
+    )
+
+    # Ages and EPC ratings
+    age_prop_chart(
+        age_data,
+        "Ffig. 9: Bandiau oedran adeiladu ac effeithlonrwydd ynni",
+        "age_prop_welsh",
+        language="welsh",
+    )
